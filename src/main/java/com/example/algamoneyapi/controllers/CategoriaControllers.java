95fbd6711038cc23917c126b1930bcc928dffed7
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-06 15:52:21 -0300

    Controllers Categoria

--- src/main/java/com/example/algamoneyapi/controllers/CategoriaControllers.java
+++ src/main/java/com/example/algamoneyapi/controllers/CategoriaControllers.java
@@ -1,9 +1,19 @@
 package com.example.algamoneyapi.controllers;
 
+import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -13,5 +23,33 @@ public class CategoriaControllers {
     @Autowired
     private CategoriaRepository repository;
 
+    @GetMapping
+    public List<Categoria> findall() {
+        return repository.findAll();
+    }
 
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> findById(@PathVariable Long codigo, HttpServletResponse response) {
+        Optional<Categoria> categoriaEncontrada = repository.findById(codigo);
+        if (categoriaEncontrada == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.of(categoriaEncontrada);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> save(@RequestBody Categoria dto, HttpServletResponse response) {
+        Categoria categoriaSave = repository.save(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSave.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSave);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void delete(@PathVariable Long codigo) {
+        repository.deleteById(codigo);
+    }
 }
